a675fc70c3f247b1be8f3a48198911f207afaf6e
Chen Nuode <chennuode2012@gmail.com>
2021-01-09 02:29:12 +0800

    work

--- flask_app.py
+++ flask_app.py
@@ -2,7 +2,7 @@
 # A very simple Flask Hello World app for you to get started with...
 import json
 from flask import Flask
-
+import random 
 app = Flask(__name__)
 
 @app.route('/')
@@ -25,7 +25,7 @@ c_dict = {
 
 @app.route('/sendmefood/<foodids>', methods=["GET"])
 def work(foodids):
-	total_calories = 81
+	total_calories = 0
 
 	foodidlist = foodids.split(",")
 	returnstring = ""
@@ -55,7 +55,54 @@ def work(foodids):
 		calorieleft = calorieleft - exercises[item]*numofreps
 		resultcount.append((item,numofreps))
 
-	returneddict = {"data" : [resultcount,resultcount]}
+	returneddict = {"workouts" : [resultcount,resultcount]}
+	loaded_r = json.dumps(returneddict)
+
+	return loaded_r
+
+
+def generate_crap():
+	foodidlist = random.sample(list(c_dict.values()),2)
+	total_calories = 0
+
+	mystr = ""
+	for item in foodidlist:
+		mystr += str(item[0]) + " || "
+		total_calories += item[1]
+	
+	#total calories that need to burned
+
+	print("bought",mystr,'total cal',str(total_calories))
+
+	#exercises Excercises, num of calories burned doing the exercise in 1 min
+	exercises = {"Push-ups":8.56/20,"Curl-ups":7.29/20,"Lunges":9.33/20,"Pull-ups":9.95/20,"Squats":0.32,"Running/km":70}
+	#running {4:,5.2, 6.7,7.5,8.6,10,12, 14}
+
+	resultcount = []
+	calorieleft = total_calories
+
+	for i, item in enumerate(exercises):
+
+		smallnum = calorieleft/(len(exercises) - i)
+		if item != "Running/km":
+			numofreps = int(smallnum/exercises[item])
+		else:
+			numofreps = round(smallnum/exercises[item],3)
+
+		calorieleft = calorieleft - exercises[item]*numofreps
+		resultcount.append((item,numofreps))
+
+	return resultcount,total_calories
+
+
+@app.route('/sendmefood2/', methods=["GET"])
+def work2():
+	returneddict = {"workouts" : [], "total_calories":[]}
+	for i in range(2):
+		w1,tc1 = generate_crap()
+		returneddict["workouts"].append(w1)
+		returneddict["total_calories"].append(tc1)
+
 	loaded_r = json.dumps(returneddict)
 
 	return loaded_r
